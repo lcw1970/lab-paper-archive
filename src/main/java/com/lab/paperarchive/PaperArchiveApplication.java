0b3bc11033e8078834311d281169fc05f335d897
sulas <lcw7010@naver.com>
2026-08-28 13:26:15 +0900

    chore: 프로젝트 초기 설정 및 프로파일 구성

---
+++ src/main/java/com/lab/paperarchive/PaperArchiveApplication.java
@@ -0,0 +1,13 @@
+package com.lab.paperarchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperArchiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaperArchiveApplication.class, args);
+    }
+
+}
